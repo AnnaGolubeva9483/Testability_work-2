b3683a3d970989f50cbbe39af2c0256001a719f6
Anna Golubeva <agolubeva9483@gmail.com>
2023-04-02 20:26:25 +0300

    Added Index

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,2 +1,6 @@
 public class BmiService {
+    public int calculate (double weightInKg, double heightInMeter) {
+        double index = weightInKg / (heightInMeter * heightInMeter);
+        return (int) index;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double weightInKg = 53.2;
+        double heightInMeter = 1.64;
+        int index = service.calculate(weightInKg, heightInMeter);
+        System.out.println(index);
+    }
 }
